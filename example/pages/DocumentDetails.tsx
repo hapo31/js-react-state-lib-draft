cce2559372a258c57d148e5937d9d4f41cfee3c7
happo31 <happo31@gmail.com>
2023-09-16 14:19:29 +0900

    feat: first commit

---
+++ example/pages/DocumentDetails.tsx
@@ -0,0 +1,35 @@
+import { documentStore } from "../store/document.store";
+
+type Props = {
+  documentId: string;
+};
+
+function DocumentDetail({ documentId }: Props) {
+  // use selectors in store defined
+  const document = documentStore.useValue(
+    ({ selectors }) => selectors.getDocumentById(documentId),
+    [documentId]
+  );
+  // use instant selector
+  const updatedAt = documentStore.useValue(
+    ({ selectors }) =>
+      format("YYYY-mm-DD", selectors.getDocumentById(documentId).updatedAt),
+    [documentId]
+  );
+  const { updateDocument } = documentStore.useMutation();
+  const [text, setText] = useState(document.text);
+
+  return (
+    <div>
+      <p>author: {document.name}</p>
+      <textarea
+        value={text}
+        onChange={({ currentTaget }) => setText(currentTaget.value)}
+      />
+      <button onClick={() => updateDocument({ ...document, text })}>
+        Update
+      </button>
+      <p>last updated: {updatedAt}</p>
+    </div>
+  );
+}
---
+++ example/store/document.store.ts
@@ -0,0 +1,51 @@
+type DocumentData = {
+  documentId: string;
+  name: string;
+  text: string;
+  updatedAt: Date;
+};
+
+type DocumentStore = {
+  documents: DocumentData[];
+};
+
+export const documentStore = createStore<DocumentStore>(
+  async () => {
+    const data = await fetch("...");
+
+    return { documents: await data.json() };
+  },
+  {
+    selectors: {
+      // store is readonly object
+      getDocumentById:
+        ({ store }) =>
+        (id: string) => {
+          return store.documents.find((document) => document.documentId === id);
+        },
+      getDocumentName:
+        ({ self }) =>
+        (id: string) => {
+          const document = self.getDocumentById(id);
+          return document.name;
+        },
+    },
+    mutations: {
+      // store is immer object
+      updateDocument:
+        ({ store }) =>
+        async (newDocument: DocumentData) => {
+          const data = await fetch("/document/update", {
+            method: "PATCH",
+            body: JSON.stringify({ newDocument }),
+          });
+          const result = await data.json();
+
+          const index = store.documents.findIndex(
+            (doc) => doc.documentId === result.documentId
+          );
+          store.documents[index] = result;
+        },
+    },
+  }
+);
